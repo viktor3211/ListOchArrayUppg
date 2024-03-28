0ba75daed6045b864294b0063e0529a0e2878d33
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-03-28 13:50:12 +0100

    Intersection

--- ListOchArrayUppg/Program.cs
+++ ListOchArrayUppg/Program.cs
@@ -64,6 +64,17 @@ namespace ListOchArrayUppg
             string[] rotateTestDataResult = Rotate(rotateTestData, 2);
             Console.WriteLine("Efter: " + string.Join(",", rotateTestDataResult));
 
+            // Intersection
+            string[] intersectionSet1 = { "a", "b", "c", "d", "e" };
+            string[] intersectionSet2 = { "h", "d", "w", "s", "a" };
+            Console.WriteLine("Demonstration av Intersection:");
+            Console.WriteLine("Set 1: " + string.Join(",", intersectionSet1));
+            Console.WriteLine("Set 2: " + string.Join(",", intersectionSet2));
+            List<string> intersectionResult = Intersection(intersectionSet1, intersectionSet2);
+            Console.WriteLine("Resultat: " + string.Join(",", intersectionResult));
+            Console.WriteLine();
+
+
         }
 
         public static int Summan(int[] listOfNumbers)
@@ -140,9 +151,23 @@ namespace ListOchArrayUppg
             {
                 result[(i + n) % strings.Length] = strings[i];
             }
-            
-
             return result;
+        }
+
+        public static List<string> Intersection(string[] set1, string[] set2)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string s1 in set1)
+            {
+                if (set2.Contains(s1))
+                {
+                    result.Add(s1);
+                }
+            }
+            return result;
+
+
         }
     }
 }
